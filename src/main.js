fa8b485af96611875d045e545a7b3fdedaade6c6
Ian <ianfarina9@gmail.com>
2025-10-14 12:02:35 -0300

    modificaciones en el CSS: cambios de fuentes, cambio en el codigo del icono, adaptacion al html. Javascript: modificacion a la pagina para limitar el proceso de carga de las secciones a cuando son observadas en pantalla.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach(el => observer.observe(el));
+
+});
